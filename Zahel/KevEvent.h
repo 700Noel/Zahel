0c5df0d2f2290793a8cafae306e5689adb656149
700Noel <leonhef@gmail.com>
2023-10-03 21:05:00 +0200

    Series: 8/123 Event System Progress: 30%

---
+++ Zahel/KevEvent.h
@@ -0,0 +1,45 @@
+#pragma once
+
+#include <string>
+
+#include "Event.h"
+
+namespace Zahel {
+
+	class ZAHEL_API KeyEvent : public Event
+	{
+	public: 
+		inline int GetKeyCode() const { return m_KeyCode; }
+
+		EVENT_CLASS_CATEGORY(EventCategoryKeyboard | EventCategoryInput)
+	protected: 
+		KeyEvent(int keycode)
+			: m_KeyCode(keycode) {}
+
+		int m_KeyCode;
+	};
+
+	class ZAHEL_API KeyPressedEvent : public KeyEvent
+	{
+	public:
+		KeyPressedEvent(int keycode, int  repeatCode)
+			: KeyEvent(keycode), m_RepeatCount(repeatCode) {}
+
+		inline int GetRepeatCount() { return m_RepeatCount; }
+
+		std::string ToString() const override
+		{
+			std::stringstream ss;
+			ss << "KeyPressedEvent: " << m_KeyCode << " (" << m_RepeatCount << " repeats)";
+			return ss.str();
+
+			EVENT_CLASS_TYPE(KeyPressed)
+		}
+
+	private:
+		int m_RepeatCount;
+	};
+
+
+
+}
