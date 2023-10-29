b190111abebd7e518b406e9e0c88abd01eacba46
700Noel <leonhef@gmail.com>
2023-10-29 18:59:25 +0100

    Series: 11/123 Window Abstraction and GLFW: Finally solved premake problems and linked GLFW (not tested yet)

--- premake5.lua
+++ premake5.lua
@@ -10,6 +10,12 @@ workspace "Zahel"
 
 outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
 
+-- Include directories relative to root folder (solution directory)
+IncludeDir = {}
+IncludeDir["GLFW"] = "Zahel/vendor/GLFW/include"
+
+include "Zahel/vendor/GLFW"
+
 project "Zahel"
 	location "Zahel"
 	kind "SharedLib"
@@ -30,7 +36,14 @@ project "Zahel"
 	includedirs
 	{
 		"%{prj.name}/src",
-		"%{prj.name}/vendor/spdlog/include"
+		"%{prj.name}/vendor/spdlog/include",
+		"%{IncludeDir.GLFW}"
+	}
+
+	links
+	{
+		"GLFW",
+		"opengl32.lib"
 	}
 
 	vpaths {
